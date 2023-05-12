de4de9f4ce24c5d5e51c6a4d52699cce93a5f4b1
DeniArif <deniarifwibowo82@gmail.com>
2023-05-12 09:59:53 +0700

    Add project files.

---
+++ exes_2_188_D/exes_2_188_D.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int arr[21];
+int n;
+int i;
+
+void input_element() {
+	
+	while (true)
+	{
+		cout << "Enter Number Of Element int the array : ";
+		cin >> n;
+		if 
+	}
+}
